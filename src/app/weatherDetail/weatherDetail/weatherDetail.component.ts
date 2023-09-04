95ee9e06a6efaef6e9a9bad6608d2054f50facba
pranav <pranav.labdhe@paynearby.in>
2023-09-04 15:43:58 +0530

    Refactoring and Code cleanup

--- src/app/weatherDetail/weatherDetail/weatherDetail.component.ts
+++ src/app/weatherDetail/weatherDetail/weatherDetail.component.ts
@@ -1,7 +1,6 @@
 import { CommonService } from './../../common.service';
-import { Component, OnInit,Input,NgZone } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import * as moment from 'moment';
 import { forkJoin } from 'rxjs';
 @Component({
   selector: 'app-weatherDetail',
@@ -9,13 +8,11 @@ import { forkJoin } from 'rxjs';
   styleUrls: ['./weatherDetail.component.scss']
 })
 export class WeatherDetailComponent implements OnInit {
-  // @Input() weatherData!: data[];
   searchCity: string = ''
   units: string = 'metric';
   currentDateTime! : string;
   searchCityPlacholder : string = 'SEARCH FOR CITY..'
   showDiv : boolean = false;
-  // searchedCity : data | null = null;
   searchedCity!: string;
   weatherData : any;
   location : string = '';
@@ -48,106 +45,11 @@ export class WeatherDetailComponent implements OnInit {
   timeOfDayImage!: string;
   onInitLat : string = '';
   onInitLong : string = '';
-  constructor(public weatherService: CommonService, private datePipe: DatePipe, private ngZone: NgZone) {
-
-    // Format date as "Friday, 12 May 2023"
-    // const currentDate = new Date();
-    // const dateOrNull: string | null = this.datePipe.transform(currentDate, 'EEEE, dd MMMM yyyy');
-    // this.formattedDate  = dateOrNull || '';
-
-    // // Format time as "11:44 AM"
-    // const timeOrNull : string | null = this.datePipe.transform(currentDate, 'hh:mm a');
-    // this.formatedTime = timeOrNull || '';
-
-    // // Check if it's day or night
-    // const timeParts = this.formatedTime.split(' ');
-    // if (timeParts.length === 2){
-    //     const time = timeParts[0];
-    //     const meridian = timeParts[1].toLowerCase();
-    //     const hours = parseInt(time.split(':')[0],10)
-
-    //     // Check Afternoon
-    //     if((meridian === 'pm' && (hours >= 12 || hours < 6 ))) {
-    //       this.checkAfterNoon = true;
-    //       this.checkMidNight = false;
-    //       this.checkNight = false;
-    //       this.checkEvening = false;
-    //       this.checkMorning = false
-    //     }
-    //     // check Evening
-    //     else if ((meridian === 'pm' && (hours >= 6 && hours < 9))) {
-    //       this.checkEvening = true;
-    //       this.checkAfterNoon = false;
-    //       this.checkNight = false;
-    //       this.checkMidNight = false;
-    //       this.checkMorning = false
-    //     }
-    //     // check Night
-    //     else if ((meridian === 'pm' && (hours >= 9 && hours < 12) )) {
-    //       this.checkNight = true;
-    //       this.checkEvening = false;
-    //       this.checkAfterNoon = false;
-    //       this.checkMidNight = false
-    //       this.checkMorning = false
-    //     }
-    //     // check Mid Night
-    //     else if ((meridian === 'am' && (hours >= 12 || hours < 4))) {
-    //       this.checkMidNight = true;
-    //       this.checkEvening = false;
-    //       this.checkAfterNoon = false;
-    //       this.checkNight = false;
-    //       this.checkMorning = false
-    //     }
-    //     // check morning
-    //     else if ((meridian === 'am' && (hours >= 4 || hours <= 11))) {
-    //       this.checkMorning = true;
-    //       this.checkNight = false;
-    //       this.checkEvening = false;
-    //       this.checkMidNight = false;
-    //       this.checkAfterNoon = false;
-    //     }
-    // }
+  constructor(public weatherService: CommonService, private datePipe: DatePipe) {
    }
 
 
   ngOnInit() {
-    // this.getLocation();
-    // const location = 'mumbai'; // Replace with the location you want to fetch weather data for
-    // this.weatherService.getOnitData(this.onInitLat,this.onInitLong).subscribe({
-    //   next:(data) => {
-    //     this.weatherData = data;
-    //     this.location = data.name;
-    //     this.temperature = data.main.temp;
-    //     this.feelsLike = data.main.feels_like
-    //     this.windDegree = data.wind.deg;
-    //     this.windSpeed = data.wind.speed;
-    //     this.humidity = data.main.humidity;
-    //     this.sunriseTime = data.sys.sunrise;
-    //     this.sunsetTime = data.sys.sunset;
-    //     this.weatherDesc = data.weather[0].description;
-    //     this.weatherMain = data.weather[0].main;
-    //     this.visibility = data.visibility / 1000;
-    //     this.pressure = data.main.pressure;
-
-    //     // this.cityLatitude = data.coord.lat;
-    //     // this.cityLongitude = data.coord.lon;
-    //     // this.convertLatToString = data.coord.lat.toString();
-    //     // this.convertLonToString = data.coord.lon.toString()
-    //     // this.cityLatitude = this.convertLatToString;
-    //     // this.cityLongitude = this.convertLonToString;
-
-    //     // console.log(this.cityLatitude);
-    //     // console.log(this.cityLongitude);
-
-
-    //     console.log(this.weatherData);
-
-    //   },
-    //   error: (error)=>{
-    //     console.log(error);
-    //   }
-    // })
-
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -277,8 +179,8 @@ export class WeatherDetailComponent implements OnInit {
               const [datePart , timePart] = this.formatedTime.split(' ')
               this.formattedDate = datePart;
               this.localCityTime = timePart
-              console.log('Date:', datePart);
-              console.log('Time:', timePart);
+              // console.log('Date:', datePart);
+              // console.log('Time:', timePart);
 
               console.log(this.formatedTime);
 
@@ -302,12 +204,6 @@ export class WeatherDetailComponent implements OnInit {
 }
 
 
-// interface data {
-//   name: string;
-//   temperature: string;
-//   wind: string;
-//   humidity: string;
-// }
 
 // http://api.timezonedb.com/v2.1/get-time-zone?key=1GJ9YWW98I18&format=json&by=position&lat=LATITUDE&lng=LONGITUDE
 // http://api.timezonedb.com/v2.1/get-time-zone?key=1GJ9YWW98I18&format=json&by=position&lat=51.5085&lng=0.1257
